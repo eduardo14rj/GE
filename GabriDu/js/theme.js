6598660b01fb3f87230606affd76fbc4e9746df7
Eduardo Oliveira <eduardo14rj@gmail.com>
2023-10-07 07:22:12 -0300

    mudanças css

--- GabriDu/js/theme.js
+++ GabriDu/js/theme.js
@@ -2,22 +2,7 @@
 function ThemeInit() {
     var theme = localStorage.getItem("theme");
 
-    function changeThemeDark(save) {
-        var body = document.querySelector("body");
-        body.classList.add("dark");
-        if(save) localStorage.setItem("theme", "dark")
-    }
-    function changeThemeLight(save) {
-        var body = document.querySelector("body");
-        body.classList.remove("dark");
-        if(save) localStorage.setItem("theme", "light")
-    }
-    function changeThemeAuto(){
-        localStorage.removeItem("theme")
-        ObserverTheme();
-    }
-
-    function ObserverTheme(){
+    function ObserverTheme() {
         if (theme == undefined) {
             if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
                 changeThemeDark(false);
@@ -25,12 +10,30 @@ function ThemeInit() {
                 changeThemeLight(false);
             }
         } else {
-            if(theme == "dark") changeThemeDark()
-            if(theme == "light") changeThemeLight()
+            if (theme == "dark") changeThemeDark()
+            if (theme == "light") changeThemeLight()
         }
     }
     ObserverTheme();
-    
+
+    function changeThemeDark(save) {
+        var body = document.querySelector("body");
+        body.classList.add("dark");
+        if (save) localStorage.setItem("theme", "dark")
+        setTimeout(() => body.style.transition = "all .3s ease", 800)
+    }
+    function changeThemeLight(save) {
+        var body = document.querySelector("body");
+        body.classList.remove("dark");
+        if (save) localStorage.setItem("theme", "light")
+        setTimeout(() => body.style.transition = "all .3s ease", 800);
+    }
+
+    function changeThemeAuto() {
+        localStorage.removeItem("theme")
+        ObserverTheme();
+    }
+
     var lightBtn = document.querySelector(".light_btn");
     var darkBtn = document.querySelector(".dark_btn");
     var autokBtn = document.querySelector(".auto_btn");

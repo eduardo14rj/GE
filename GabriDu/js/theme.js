89829cbfa6a9b74c2fdfa2ada5f85629c0f60d91
Eduardo Oliveira <eduardo14rj@gmail.com>
2023-10-20 02:04:10 -0300

    tema com nome e finalizado documentação style.scss

--- GabriDu/js/theme.js
+++ GabriDu/js/theme.js
@@ -1,8 +1,70 @@
 
+// function ThemeInit() {
+//     var body = document.querySelector("body");
+//     var ball = document.querySelector("[switch-ball]")
+
+//     function ObserverTheme() {
+//         var theme = localStorage.getItem("theme");
+//         if (theme == undefined) {
+//             if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//                 changeThemeDark();
+//             } else {
+//                 changeThemeLight();
+//             }
+//             setBallClass("auto");
+//         } else {
+//             if (theme == "dark") changeThemeDark()
+//             if (theme == "light") changeThemeLight()
+//         }
+//     }
+
+//     ObserverTheme();
+
+//     function changeThemeDark(save) {
+//         body.classList.add("dark");
+//         setBallClass("dark")
+//         if (save) localStorage.setItem("theme", "dark")
+//         setTimeout(() => body.style.transition = "all .3s ease", 800)
+//     }
+
+//     function changeThemeLight(save) {
+//         body.classList.remove("dark");
+//         setBallClass("light")
+//         if (save) localStorage.setItem("theme", "light")
+//         setTimeout(() => body.style.transition = "all .3s ease", 800);
+//     }
+
+//     function changeThemeAuto() {
+//         localStorage.removeItem("theme")
+//         ObserverTheme();
+//         setBallClass("auto")
+//     }
+
+//     function setBallClass(classe) {
+//         ball.classList.remove.apply(ball.classList, Array.from(ball.classList));
+//         ball.classList.add(classe);
+//     }
+
+//     var lightBtn = document.querySelector(".light_btn");
+//     var darkBtn = document.querySelector(".dark_btn");
+//     var autokBtn = document.querySelector(".auto_btn");
+
+//     lightBtn.addEventListener("click", changeThemeLight);
+//     darkBtn.addEventListener("click", changeThemeDark);
+//     autokBtn.addEventListener("click", changeThemeAuto);
+// }
+
+// ThemeInit();
+
 function ThemeInit() {
     var body = document.querySelector("body");
-    var ball = document.querySelector("[switch-ball]")
-    
+    var switchActived = document.querySelector(".switch-actived");
+    var switchDropdown = document.querySelector(".switch-dropdown");
+
+    var btn_switch_light = document.querySelector(".switch-light");
+    var btn_switch_dark = document.querySelector(".switch-dark");
+    var btn_switch_auto = document.querySelector(".switch-auto");
+
     function ObserverTheme() {
         var theme = localStorage.getItem("theme");
         if (theme == undefined) {
@@ -11,47 +73,58 @@ function ThemeInit() {
             } else {
                 changeThemeLight();
             }
-            setBallClass("auto");
         } else {
             if (theme == "dark") changeThemeDark()
             if (theme == "light") changeThemeLight()
         }
     }
-
     ObserverTheme();
+
+    function setActive(theme) {
+        var span = switchActived.querySelector(".material-symbols-outlined");
+        switch (theme) {
+            case "light":
+                span.textContent = "light_mode";
+                break;
+            case "dark":
+                span.textContent = "dark_mode";
+                break;
+            default:
+                span.textContent = "tonality";
+                break;
+        }
+    }
 
     function changeThemeDark(save) {
         body.classList.add("dark");
-        setBallClass("dark")
         if (save) localStorage.setItem("theme", "dark")
         setTimeout(() => body.style.transition = "all .3s ease", 800)
+        switchDropdown.classList.remove("active");
+        setActive("dark");
     }
 
     function changeThemeLight(save) {
         body.classList.remove("dark");
-        setBallClass("light")
         if (save) localStorage.setItem("theme", "light")
         setTimeout(() => body.style.transition = "all .3s ease", 800);
+        switchDropdown.classList.remove("active");
+        setActive("light");
     }
 
     function changeThemeAuto() {
         localStorage.removeItem("theme")
         ObserverTheme();
-        setBallClass("auto")
+        switchDropdown.classList.remove("active");
+        setActive("auto");
     }
 
-    function setBallClass(classe) {
-        ball.classList.remove.apply(ball.classList, Array.from(ball.classList));
-        ball.classList.add(classe);
-    }
+    //Abrir o dropdown
+    switchActived.addEventListener("click", () => {
+        switchDropdown.classList.toggle("active");
+    })
 
-    var lightBtn = document.querySelector(".light_btn");
-    var darkBtn = document.querySelector(".dark_btn");
-    var autokBtn = document.querySelector(".auto_btn");
-
-    lightBtn.addEventListener("click", changeThemeLight);
-    darkBtn.addEventListener("click", changeThemeDark);
-    autokBtn.addEventListener("click", changeThemeAuto);
+    btn_switch_light.addEventListener("click", changeThemeLight);
+    btn_switch_dark.addEventListener("click", changeThemeDark);
+    btn_switch_auto.addEventListener("click", changeThemeAuto);
 }
-
 ThemeInit();
--- GabriDu/styles/readme.md
+++ GabriDu/styles/readme.md
@@ -1,21 +1,21 @@
 ![Logo](../favicon.ico)
 # Documentação CSS/SCSS do projeto GE
 
-O projeto **GE** é um portfólio criado com as principais linguagens para a criação web. Como o projeto está incluso em um grupo, é necessário uma organização de documentos que sejam autoexplicativos em seus nomes.
+O projeto **GE** é um portfólio que engloba as principais linguagens de criação web. Como faz parte de um grupo, é fundamental manter uma organização dos documentos, garantindo que seus nomes sejam autoexplicativos.
 
 Neste documento, será explicada a organização dos documentos de estilização **CSS** e **SCSS**.
 
 ## Documentos
-A organização de documentos é importante para a redução de códigos em um único documento e para facilitar a detecção de trechos de código.
-
-Além disso, torna-se fácil o entendimento não apenas do grupo de programadores, mas também dos futuros programadores, seja porque o grupo cresça ou seja substituído.
-
-Abaixo, é explicado o que é cada documento **.css** ou **.scss** dentro da pasta styles.
+>A organização de documentos é importante para a redução de códigos em um único documento e para facilitar a detecção de trechos de código.
+>
+>Além disso, torna-se fácil o entendimento não apenas do grupo de programadores, mas também dos futuros programadores, seja porque o grupo cresça ou seja substituído.
+>
+>Abaixo, é explicado o que é cada documento **.css** ou **.scss** dentro da pasta styles.
 
 ### index.scss
->Documento que contém todas as importações de outros documentos .scss, com o objetivo de gerar um documento único **index.css**, sendo chamado pelos documentos **.html**.
->
->Para isso, é necessário instalar um extensor que possibilite a compilação e da geração do documento **.css** único, utilizamos o [Live Sass Compiler](https://marketplace.visualstudio.com/items?itemName=glenn2223.live-sass)
+Documento que contém todas as importações de outros documentos .scss, com o objetivo de gerar um documento único **index.css**, sendo chamado pelos documentos **.html**.
+
+Para isso, é necessário instalar um extensor que possibilite a compilação e da geração do documento **.css** único, utilizamos o [Live Sass Compiler](https://marketplace.visualstudio.com/items?itemName=glenn2223.live-sass)
 para isso.
 
 ```html
@@ -23,7 +23,7 @@ para isso.
 ```
 
 ### _config.scss
->Documento que tem a utilidade de armazenar variáveis, como cores, colunas, valores de espaçamentos e outras variáveis. 
+Documento que tem a utilidade de armazenar variáveis, como cores, colunas, valores de espaçamentos e outras variáveis. 
 ```scss
     $colors: (
     primary: #14E3D9,
@@ -36,13 +36,100 @@ para isso.
         25: .25
     );
 ```
->E para utilizar essas variáveis dentro de **_config.css**, é necessário importar o documento no documento **.scss** desejado.
+E para utilizar essas variáveis dentro de **_config.css**, assim como todos os outros documentos, é necessário importar o documento no lugar desejado.
 
 ```scss
     @import './config';
 ```
 ### _system.scss
->Documento que define as estilizações principais dos elementos HTML, como definições de fontes, tamanhos e cores bases dos elementos, baseados no **Design System** do projeto.
+Documento que define as estilizações principais dos elementos HTML, como definições de fontes, tamanhos e cores bases dos elementos, baseados no **Design System** do projeto.
+
+```scss
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+label,
+a,
+span {
+    font-family: "Inter", "Lato", sans-serif;
+}
+
+h1 {
+    font-size: 48px;
+    line-height: 64px;
+}
+
+h2 {
+    font-size: 38px;
+    line-height: 46px;
+}
+```
+
 
 
 ### _utils.scss
+Um documento responsável em armazenar pacotes de utilidades para a criação de outra estilizações futuras. Isso ajuda outros documentos de estilizações do projeto, servindo como suporte de pacote de funções e **@includes**.
+ ```scss
+ @mixin mobile() {
+    @media screen and (max-width: 762px) {
+        @content;
+    }
+}
+@mixin web(){
+    @media (min-width: 762px) {
+        @content;
+    }
+}
+ ```
+
+### _classes.scss
+Documento que armazena e defina uma série de pacotes de classes prontas para o uso no **.html**. O documento inclusive converta as variáveis armazenadas no documento **_config.scss** em classes únicas ou combinadas usáveis.
+
+>Exemplo da classe definida e convertida em css no index.css:
+```css
+.m-3 {
+  margin: 2rem;
+}
+
+.p-3 {
+  padding: 2rem;
+}
+
+.pt-3 {
+  padding-top: 2rem;
+}
+
+.mt-3 {
+  margin-top: 2rem;
+}
+
+.pb-3 {
+  padding-bottom: 2rem;
+}
+
+.mb-3 {
+  margin-bottom: 2rem;
+}
+
+.pl-3 {
+  padding-left: 2rem;
+}
+
+.ml-3 {
+  margin-left: 2rem;
+}
+
+.pr-3 {
+  padding-right: 2rem;
+}
+
+.mr-3 {
+  margin-right: 2rem;
+}
+```
+
+### _components.scss
+Documento de estilização livre, que também serve para estilizações específicos das páginas em geral.

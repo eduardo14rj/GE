fbd0d6644a943d3e971b270ff5c12bf5d3e7fc3e
Eduardo Oliveira <eduardo14rj@gmail.com>
2023-11-18 00:34:20 -0300

    skills

--- GabriDu/js/scrollNavigation.js
+++ GabriDu/js/scrollNavigation.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
@@ -52,7 +52,7 @@ function handleScroll(){
             navLinks.forEach(link => {
                 link.classList.remove('active');
             });
-            navLinks[index].classList.add('active');
+            navLinks[index]?.classList.add('active');
         }
     });
 }
